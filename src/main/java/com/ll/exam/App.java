908b8fa83104bce7b8fa78175c5da0c95b82f273
leewooyup <zjadlspun1114@naver.com>
2022-08-04 11:10:05 +0900

    ArticleServiceTest 추가

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -3,8 +3,8 @@ package com.ll.exam;
 public class App {
     public static final String BASE_PACKAGE_PATH = "com.ll.exam";
     public static final String DB_HOST = "localhost";
-    public static final int DB_PORT = 3306;
-    public static final String DB_ID = "lldj";
-    public static final String DB_PASSWORD = "lldj1234";
-    public static final String DB_NAME = "a9";
+    public static final int DB_PORT = 3307;
+    public static final String DB_ID = "leewooyup";
+    public static final String DB_PASSWORD = "leewooyup123";
+    public static final String DB_NAME = "blog";
 }
